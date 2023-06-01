e99a6edf3a4bdef110f394e2bdd2e0e6e58bee80
liliang <759628324@qq.com>
2023-06-01 17:29:36 +0800

    sse demo init

---
+++ src/main/java/com/liliang/springbootsse/SpringbootSseApplication.java
@@ -0,0 +1,13 @@
+package com.liliang.springbootsse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSseApplication.class, args);
+    }
+
+}
